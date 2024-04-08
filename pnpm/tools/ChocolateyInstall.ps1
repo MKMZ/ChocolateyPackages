97a04c8e8ae00cad2f41738c75337ee8b8652fe0
Rafał Klimek <rk.mkmz@gmail.com>
2024-04-08 16:13:24 +0200

    pnpm: Fix process of updating package

--- pnpm/tools/ChocolateyInstall.ps1
+++ pnpm/tools/ChocolateyInstall.ps1
@@ -8,24 +8,6 @@ $architecture = 'x64'
 $platform = 'win'
 $pnpmExecutableFileName = "$packageName.exe"
 
-$pnpmCommand = Get-Command -Name $packageName -ErrorAction SilentlyContinue
-if ($null -ne $pnpmCommand -and (-not $env:ChocolateyForce))
-{
-    $currentPnpmVersionString = pnpm --version
-    $currentPnpmVersion = [version]::new()
-    $isValidCurrentVersion = [version]::TryParse($currentPnpmVersionString, [ref]$currentPnpmVersion)
-    if (-not $isValidCurrentVersion)
-    {
-        Write-Error "Invalid current version of pnpm detected: ""$currentPnpmVersionString""."
-        return
-    }
-    if ($packagePnpmVersion -le $currentPnpmVersion)
-    {
-        Write-Host "This pnpm package version '$packagePnpmVersion' is lower or equal to currently installed pnpm version '$currentPnpmVersion'. Use the --force switch to install version $packagePnpmVersion anyway."
-        return
-    }
-}
-
 if ([System.Environment]::Is64BitOperatingSystem -eq $false) {
 	Write-Error "pnpm currently only provides binaries for x64 architectures on Windows are available. Try to install pnpm using npm."
 	return
